c13b888ecc0e625aa8cd0df1f3ab690837319a55
Agus Hidayatullah <agushidayatullah@MacBook-Air-Agus.local>
2026-08-09 04:15:02 +0700

    fix: GTM use raw script tag instead of Next.js Script (debug compat)

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -55,9 +55,7 @@ export default function RootLayout({
     <html lang="id">
       <head>
         {/* Google Tag Manager */}
-        <Script id="gtm-base" strategy="afterInteractive">
-          {`(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src='https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);})(window,document,'script','dataLayer','${GTM_ID}');`}
-        </Script>
+        <script dangerouslySetInnerHTML={{ __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src='https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);})(window,document,'script','dataLayer','${GTM_ID}');` }} />
         {/* End Google Tag Manager */}
       </head>
       <body className="min-h-screen flex flex-col">
